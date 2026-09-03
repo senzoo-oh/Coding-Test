5f7a11f2da5848fcfe9e1bdb56a5dc1c49328676
senzoo-oh <osj3418@gmail.com>
2026-09-03 22:10:21 +0900

    20260903

---
+++ PG/지게차와크레인/20260903.java
@@ -0,0 +1,123 @@
+import java.util.*;
+
+class Solution {
+    
+    public class Container {
+        int r;
+        int c;
+        
+        Container(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+    
+    public int[][] dirs = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    public char[][] storages;
+    
+    public int row;
+    public int col;
+    
+    public char[][] status;
+    public boolean[][] visited;
+    
+    public int solution(String[] storage, String[] requests) {
+        int answer = 0;
+        
+        // storages를 인덱스 2씩 늘려서 창고가 배열의 정중앙에 오게 구현
+        row = storage.length + 2;
+        col = storage[0].length() + 2;
+        
+        storages = new char[row][col];
+        
+        for (int r = 1; r < row - 1; r++) {
+            for (int c = 1; c < col - 1;  c++) {
+                storages[r][c] = storage[r-1].charAt(c-1);
+            }
+        }
+        
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col;  c++) {
+                if (r == 0 || r == row - 1 || c == 0 || c == col - 1) storages[r][c] = '.';
+            }
+        }
+        
+        // 각 요청에 대해서
+            // 상태 배열 생성
+            
+            // BFS(0, 0) 으로 외부와 연결된 컨테이너를 제거함
+        
+            // 크레인을 사용하는 경우
+                // 모든 칸을 돌면서 요청이 들어온 알파벳이면 제거함
+        for (int req = 0; req < requests.length; req++) {
+            status = new char[row][col];
+            
+            BFS(0, 0, requests[req]);
+            
+            if (requests[req].length() == 2) {
+                for (int r = 1; r < row - 1; r++) {
+                    for (int c = 1; c < col - 1; c++) {
+                        if (storages[r][c] == requests[req].charAt(0)) {
+                            status[r][c] = '.';
+                        }
+                    }
+                }
+            }
+            
+            for (int r = 1; r < row - 1; r++) {
+                for (int c = 1; c < col - 1; c++) {
+                    if (status[r][c] == '.') {
+                        storages[r][c] = status[r][c];
+                    }
+                }
+            }
+        }
+        
+        for (int r = 1; r < row - 1; r++) {
+            for (int c = 1; c < col - 1; c++) {
+                if (storages[r][c] != '.') answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void BFS(int r, int c, String req) {
+        Queue<Container> queue = new LinkedList<>();
+        visited = new boolean[row][col];
+        
+        queue.add(new Container(r, c));
+        visited[r][c] = true;
+        
+        while (!queue.isEmpty()) {
+            Container cur = queue.poll();
+            
+            for (int d = 0; d < 4; d++) {
+                int nr = cur.r + dirs[d][0];
+                int nc = cur.c + dirs[d][1];
+                
+                if (nr < 0 || nc < 0 || row <= nr || col <= nc) continue;
+                if (visited[nr][nc]) continue;
+                
+                // 현재 외부 주위에 요청이 들어온 컨테이너가 있으면 -> 상태 업데이트
+                if (storages[nr][nc] == req.charAt(0)) {
+                    status[nr][nc] = '.';
+                }
+                
+                // 현재 외부와 연결된 외부가 있다면 큐에 담고 탐색함
+                else if (storages[nr][nc] == '.') {
+                    queue.add(new Container(nr, nc));
+                    visited[nr][nc] = true;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
